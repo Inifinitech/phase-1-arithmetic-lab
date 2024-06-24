8a9f57e28d2536de047fef3cc8c016e1a58d90ea
Izaack Kuria <kuriaisac@gmail.com>
2024-06-24 23:46:20 +0300

    Addsarithmetic methods

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 10;
+let num2 = 6.2;
+let num3 = 22;
+let num4 = 18;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(2, 3, 3, 17, 20, 19, 13);
